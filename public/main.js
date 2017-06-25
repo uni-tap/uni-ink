8a0c2a17d49f73d0f995c3593f5bffa899e06b62
uni-tap <divg809@gmail.com>
2017-06-25 11:29:17 +0530

    Update main.js

--- public/main.js
+++ public/main.js
@@ -709,6 +709,20 @@ function move_right(){
     current.tool = "Pen";
     document.body.requestFullscreen();
     }
+	function toggleFullScreen() {
+  var doc = window.document;
+  var docEl = doc.documentElement;
+
+  var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
+  var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
+
+  if(!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
+    requestFullScreen.call(docEl);
+  }
+  else {
+    cancelFullScreen.call(doc);
+  }
+}
     undo.addEventListener('click', function() {
      history.undo(canvas, context, true);
    });
