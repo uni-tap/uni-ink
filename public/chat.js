164b0eacd851f6d3b616b50e670d53da23eda750
uni-tap <divg809@gmail.com>
2018-04-15 21:59:55 +0530

    Add files via upload

---
+++ public/chat.js
@@ -0,0 +1,150 @@
+var filters = {
+    profanity : function(dat){
+         return true
+    },
+    markup : function(){
+        
+    },
+    emoji : function(){
+        
+    },
+    blocked_usr : function (){
+        
+    },
+    from_blocked : function(usr){
+        console.log('filtering');
+        for (var i = 0; i < blocked.length; i++) {
+        if(usr == blocked[i]){
+            window.reload();
+        }
+        }
+    },
+    links : function(){
+        return true
+    }
+}
+var emomsgs = [];
+var msg = document.getElementById("msg");
+var send = document.getElementById("send");
+var msg_lst = document.getElementById('chats');
+    send.addEventListener('click', function(){
+        send_msg();
+    },false);
+
+function send_msg(type){
+    var li = document.createElement('li');
+    if(!type || type == 'norm'){
+        if(msg.value == ''){return;}
+        li.className = 's_msg';
+        li.style.backgroundColor = document.getElementById('cht_clr').innerHTML;
+        li.innerHTML = msg.value;
+    }else if(type == 'img'){
+        li.className = 's_msg s_img';
+        li.style.backgroundColor = document.getElementById('cht_clr').innerHTML;
+        var img_box = document.createElement('img');
+            img_box.src = "http://uni-tap.co/nusr/dp.php?id=58";
+            li.appendChild(img_box);  
+        //li.innerHTML = msg.value;
+    }else if(type == 'emoji'){
+        li.className = 's_msg s_emoji';
+        li.style.backgroundColor = document.getElementById('cht_clr').innerHTML;
+        var img_box = document.createElement('img');
+            img_box.src = "https://static.xx.fbcdn.net/images/emoji.php/v9/z88/1/32/1f600.png";
+            li.appendChild(img_box);
+    }else if(type == 'link'){
+        li.className = 's_msg s_link';
+        li.style.backgroundColor = document.getElementById('cht_clr').innerHTML;
+        var prev = document.createElement('iframe');
+            prev.src = "/security.html";
+            li.appendChild(prev);
+    }else if(type == 'animoji'){
+        li.className = 's_msg s_aemoji';
+        li.setAttribute('emo_cont', msg.value);
+        li.style.backgroundColor = document.getElementById('cht_clr').innerHTML;
+        animate();  
+    }
+    var info_div = document.createElement('div');
+        info_div.className = 'usr_info';
+    var usr_img = document.createElement('img');
+        usr_img.src = "http://uni-tap.co/nusr/dp.php?id=1";
+        info_div.appendChild(usr_img);
+    var info = document.createElement('span');
+        info.innerHTML += 'You';
+        info_div.appendChild(info);
+        li.appendChild(info_div);
+    msg_lst.appendChild(li);
+    emojify.run();
+// OR
+emojify.run(document.getElementById('msg'));
+// OR
+emojify.run(null, function(emoji, emojiName){
+  var span = document.createElement('span');
+  span.className = 'emoji emoji-'  + emojiName;
+  span.innerHTML = emoji + ' replaced';
+  return span;
+});
+    li.focus();
+    msg.value = '';
+    toggle('.placeholder');
+}
+function recieve_msg(r_msg, type, from){
+    var validate = filters.from_blocked(from);
+    validate;
+    var li = document.createElement('li');
+    if(!type || type == 'norm'){
+        li.className = 'r_msg';
+        li.innerHTML = r_msg;
+    }else if(type == 'img'){
+        li.className = 'r_msg s_img';
+        var img_box = document.createElement('img');
+            img_box.src = "http://uni-tap.co/nusr/dp.php?id=58";
+            li.appendChild(img_box);  
+        //li.innerHTML = msg.value;
+    }else if(type == 'emoji'){
+        li.className = 's_msg s_emoji';
+        var img_box = document.createElement('img');
+            img_box.src = "https://static.xx.fbcdn.net/images/emoji.php/v9/z88/1/32/1f600.png";
+            li.appendChild(img_box);
+    }else if(type == 'link'){
+        li.className = 's_msg s_link';
+        var prev = document.createElement('iframe');
+            prev.src = "/security.html";
+            li.appendChild(prev);
+    }
+    var info_div = document.createElement('div');
+        info_div.className = 'usr_info';  
+    var usr_img = document.createElement('img');
+        usr_img.src = "http://uni-tap.co/nusr/dp.php?id=1";
+        usr_img.addEventListener('click', function(){options(usr_img, from)},false);
+        info_div.appendChild(usr_img);
+    var info = document.createElement('span');
+        info.innerHTML += from;
+        info_div.appendChild(info);
+        li.appendChild(info_div);
+        li.setAttribute('data-usr', from);
+    msg_lst.appendChild(li);
+    emojify.run();
+// OR
+emojify.run(document.getElementById('msg'));
+// OR
+emojify.run(null, function(emoji, emojiName){
+  var span = document.createElement('span');
+  span.className = 'emoji emoji-'  + emojiName;
+  span.innerHTML = emoji + ' replaced';
+  return span;
+});
+}   
+    function animate(){  
+        var emsgs = document.getElementsByClassName('s_aemoji');
+        var s = msg.value;
+    for(i = 0; i < emsgs.length; i++){   
+        var emo = msg.value;
+        if(emo == 'undefined'){emo = "";}
+      setInterval(function(){
+  emsgs[i].innerHTML = [
+    ...emo
+  ][~~(Math.random()*6)]
+},500);
+      console.log('running');
+    }
+    }
---
+++ public/imports.js
@@ -0,0 +1,19 @@
+var frame = document.getElementById('fileFrame');
+var url = document.getElementById('frameURL');
+var load = document.getElementById('loadFrameURL');
+    load.addEventListener('click', youTube, false);
+function youTube(){
+    frame.src = url.value;
+}
+function system(){
+    
+}
+function gDrive(){
+    
+}
+function dropBox(){
+    
+}
+function oneDrive(){
+    
+}
