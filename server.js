de3e7fe3bdb4ed8797f9a67209f4670377a794f8
uni-tap <divg809@gmail.com>
2018-04-22 21:09:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -47,6 +47,7 @@ io.on('connection', function(socket){
   socket.on('chat_msg', (data)  => socket.broadcast.emit('chat_msg', data));
   socket.on('user_attached', (data)  => socket.broadcast.emit('user_attached', data));
   socket.on('youtube_import_uni-tap', (data)  => socket.broadcast.emit('youtube_import_uni-tap', data));
+  socket.on('drags', (data)  => socket.broadcast.emit('drags', data));
 // IF A USER GETS DISCONNECTED
 
 socket.on('disconnect', function(socket){
