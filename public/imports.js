f975431d5b46b28b017db46d838f9fb57aed062a
uni-tap <divg809@gmail.com>
2018-04-22 14:05:21 +0530

    Update imports.js

--- public/imports.js
+++ public/imports.js
@@ -3,7 +3,17 @@ var url = document.getElementById('frameURL');
 var load = document.getElementById('loadFrameURL');
     load.addEventListener('click', youTube, false);
 function youTube(){
-    frame.src = url.value;
+     var SearchTerm = 'watch?v=';
+     var TextSearch = url.value;
+
+  if (SearchTerm.length > 0 && TextSearch.indexOf(SearchTerm) > -1) {
+    alert("String Found. Search Complete");
+    var text = url.value;
+    text.replace(SearchTerm, "embed")  
+    frame.src = text;  
+  } else {
+    alert("No Data found in Text Area");
+  }
 }
 function system(){
     
