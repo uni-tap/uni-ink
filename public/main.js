c52b8e6d75a481fd96b8687c55f2fa18f37b8bda
uni-tap <divg809@gmail.com>
2018-04-22 13:40:49 +0530

    Update main.js

--- public/main.js
+++ public/main.js
@@ -331,10 +331,10 @@ window.onload = function(){
     return color;
   }
   function getRandomId() {
-    var letters = '0123456789ABCDEF';
+    var letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
     var color = '';
     for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
+      color += letters[Math.floor(Math.random() * 52)];
     }
     return color;
   }    
