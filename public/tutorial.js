78a514cd50677ab2d8b61905e368d51d52c244d4
uni-tap <divg809@gmail.com>
2018-05-04 13:25:51 +0530

    Add files via upload

---
+++ public/tutorial.js
@@ -0,0 +1,14 @@
+'use strict';
+(function() {
+    var toolbox = document.querySelector('.tls_nav');
+    var npagebox = document.querySelector('.board-top-left-panel');
+    var endbox = document.querySelector('.board-top-right-panel');
+    var vidbox = document.querySelector('.video-chat');
+    var chatbox = document.querySelector('.chat_body');
+    var btmbar = document.querySelector('.bottom-bar');
+    var undobox = document.querySelector('.undo-redo');
+    
+    //set 1
+    
+    toolbox.style.transform = 'translate(853px, -9px)';
+});
