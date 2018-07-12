3b45f383f1f6be34e226f92340c3621ca6d3c63f
uni-tap <divg809@gmail.com>
2018-07-12 21:37:01 +0530

    Update init.js

--- public/init.js
+++ public/init.js
@@ -1,8 +1,8 @@
 (function () {
-var nav = document.querySelector('.tls_nav');
+var nav = document.querySelector('.toolbar__panel--draw');
 
 var new_elm = document.createElement('div');
-new_elm.className += " toolbar__item toolbar__item--customized";
+new_elm.className += " toolbar__panel__item";
 new_elm.innerHTML = "hloo";
 nav.appendChild(new_elm);
 })();
