a8de30c99aedecf5912fb2a55d46a5f8a155a52e
uni-tap <divg809@gmail.com>
2018-07-12 21:26:51 +0530

    Create init.js

---
+++ public/init.js
@@ -0,0 +1,6 @@
+var nav = document.querySelector('.tls_nav');
+
+var new_elm = document.createElement(div);
+new_elm.className += " toolbar__item toolbar__item--customized";
+new_elm.innerHTML = "hloo";
+nav.appendChild(new_elm);
