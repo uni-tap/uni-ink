c466a794cb698d7b2fc29e8af30e8eaccaab1117
uni-tap <divg809@gmail.com>
2018-07-12 21:40:34 +0530

    Update init.js

--- public/init.js
+++ public/init.js
@@ -1,9 +1,10 @@
 (function () {
 var nav = document.querySelector('.toolbar__panel--draw');
-
+var elmnt = uni_tap.createpage({cont:"<div class=wlcm > <h2> Welcome </h2> </div>"}); 
 var new_elm = document.createElement('div');
 new_elm.className += " toolbar__panel__item";
 new_elm.innerHTML = "hloo";
-  new_elm.onclick = function(){alert("new elm")}
+document.body.appendChild(elmnt);  
+new_elm.onclick = function(){alert("new elm")}
 nav.appendChild(new_elm);
 })();
