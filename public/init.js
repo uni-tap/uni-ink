e9c7325250769b9b812667968c3f3a87ace3a585
uni-tap <divg809@gmail.com>
2018-07-13 21:32:49 +0530

    Create init.js

---
+++ public/init.js
@@ -0,0 +1,8 @@
+var scr = document.createELement("script");
+scr.setAttribute("src", "addons/pan.js");
+
+var scr2 = document.createELement("script");
+scr2.setAttribute("src", "addons/timer.js");
+
+document.body.appendChild(scr);
+document.body.appendChild(scr2);
