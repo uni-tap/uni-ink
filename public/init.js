d8475ca808a81cb553137c65783337341e934f0b
uni-tap <divg809@gmail.com>
2018-07-13 22:15:52 +0530

    Update init.js

--- public/init.js
+++ public/init.js
@@ -7,6 +7,10 @@ scr2.setAttribute("src", "addons/timer.js");
 var scr3 = document.createElement("script");
 scr3.setAttribute("src", "addons/moveobjects.js");
 
+var scr4 = document.createElement("script");
+scr4.setAttribute("src", "addons/grid.js");
+
 document.body.appendChild(scr);
 document.body.appendChild(scr2);
 document.body.appendChild(scr3);
+document.body.appendChild(scr4);
