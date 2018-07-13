285710320ece7aedd526330cd11234a1d720420d
uni-tap <divg809@gmail.com>
2018-07-13 21:57:42 +0530

    Update init.js

--- public/init.js
+++ public/init.js
@@ -4,5 +4,9 @@ scr.setAttribute("src", "addons/pan.js");
 var scr2 = document.createElement("script");
 scr2.setAttribute("src", "addons/timer.js");
 
+var scr3 = document.createElement("script");
+scr3.setAttribute("src", "addons/moveobjects.js");
+
 document.body.appendChild(scr);
 document.body.appendChild(scr2);
+document.body.appendChild(scr3);
