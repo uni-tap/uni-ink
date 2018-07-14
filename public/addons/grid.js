c88b87e4ab796cd193f3a366bab9129a49f3fbb4
uni-tap <divg809@gmail.com>
2018-07-14 12:54:39 +0530

    Update grid.js

--- public/addons/grid.js
+++ public/addons/grid.js
@@ -4,7 +4,7 @@ var drawGrid = function(w, h, id) {
     ctx.canvas.width  = w;
     ctx.canvas.height = h;
     
-    var data = '<svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg"> \
+    var data = '<svg width="1000%" height="1000%" xmlns="http://www.w3.org/2000/svg"> \
         <defs> \
             <pattern id="smallGrid" width="8" height="8" patternUnits="userSpaceOnUse"> \
                 <path d="M 8 0 L 0 0 0 8" fill="none" stroke="gray" stroke-width="0.5" /> \
